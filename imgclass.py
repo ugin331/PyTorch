1b1a1e743e34066e06add9752d917a8f0c30dc3b
who, me? <yuanren.li@gmail.com>
2020-06-17 13:39:04 -0700

    minor tweak for readability

--- imgclass.py
+++ imgclass.py
@@ -16,20 +16,20 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=False, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
-
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=2)
-
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 
 # functions to show an image
 def imshow(img):
     img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
 
 # define neural network
 class Net(nn.Module):
@@ -106,5 +106,6 @@ def run():
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 
+# main function so it doesn't throw a multithread error
 if __name__ == '__main__':
     run()
