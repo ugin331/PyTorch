d1b66766ea91e4d77c0c9e59c3eabfebbcec8ae1
who, me? <yuanren.li@gmail.com>
2020-06-18 21:43:06 -0700

    Fixed image classification not working

--- imgclasstest.py
+++ imgclasstest.py
@@ -1,21 +1,15 @@
-import torch as torch
+import torch
 import torchvision
-import torchvision.transforms as transforms
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
+import torchvision.transforms as transforms
 
-# load CIFAR10 dataset
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
@@ -23,13 +17,11 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
 # functions to show an image
 def imshow(img):
     img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
 
 # define neural network
 class Net(nn.Module):
@@ -53,61 +45,21 @@ class Net(nn.Module):
 
 
 def run():
-    torch.multiprocessing.freeze_support()
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
-
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-    net = Net()
-
-    # loss func and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(2):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-    print('Finished Training')
-
     PATH = './cifar_net.pth'
-    torch.save(net.state_dict(), PATH)
-
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
     dataiter = iter(testloader)
     images, labels = dataiter.next()
+    outputs = net(images)
 
     # print images
     imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
     print(labels)
     _, predicted = torch.max(outputs, 1)
-    print(predicted)
+    print(predicted[0].data)
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 
-# main function so it doesn't throw a multithread error
 if __name__ == '__main__':
     run()
