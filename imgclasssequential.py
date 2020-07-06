e2663f6c4f0194ba61e2eee51981ec8e19aad031
who, me? <yuanren.li@gmail.com>
2020-07-05 17:40:14 -0700

    possibly complete modular cnn

--- imgclasssequential.py
+++ imgclasssequential.py
@@ -94,10 +94,12 @@ def run():
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+                print('lol')
+            #     print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            #     running_loss = 0.0
 
-    print('Finished Training')
+    # average training loss
+    print(running_loss/len(trainloader))
 
     PATH = './cifar_seq_net.pth'
     torch.save(net.state_dict(), PATH)
--- modularcnn.py
+++ modularcnn.py
@@ -5,22 +5,22 @@ import numpy as np
 
 
 class testNet(nn.Module):
-    def __init__(self, n_in, layer1_conv_out, ):
+    def __init__(self, n_in, layer1_conv_out, layer2_conv_out, conv_size, linear1_out, linear2_out, final_out):
         super(testNet, self).__init__()
         self.conv_net = nn.Sequential(
-            nn.Conv2d(n_in, layer1_conv_out, 5),
+            nn.Conv2d(n_in, layer1_conv_out, conv_size),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(6, 16, 5),
+            nn.Conv2d(layer1_conv_out, layer2_conv_out, conv_size),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
         self.linear = nn.Sequential(
-            nn.Linear(16 * 5 * 5, 120),
+            nn.Linear(layer2_conv_out * conv_size * conv_size, linear1_out),
             nn.ReLU(),
-            nn.Linear(120, 84),
+            nn.Linear(linear1_out, linear2_out),
             nn.ReLU(),
-            nn.Linear(84, 10)
+            nn.Linear(linear2_out, final_out)
         )
 
     def forward(self, x):
